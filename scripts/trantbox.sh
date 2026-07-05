9419ba02ea216f2941a0bdfd0afeb27d5f8c4c1b
TranThangBin <thangdev04@gmail.com>
2026-07-05 14:18:05 +0700

    refactor: just have the base image with default dependencies

---
+++ scripts/trantbox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./distrobox-shims.sh
+
+grep -v "^#" ./trantbox-base.packages | xargs pacman -Syyu --needed --noconfirm
+
+wget https://raw.githubusercontent.com/Shringe/LatencyFleX-Installer/main/install.sh -O /usr/bin/latencyflex
+sed -i 's@"dxvk.conf"@"/usr/share/latencyflex/dxvk.conf"@g' /usr/bin/latencyflex
+chmod +x /usr/bin/latencyflex
+
+yes | pacman -Scc
+rm -rf /tmp/* /var/cache/pacman/pkg/*
+sed -i 's/-march=x86-64 -mtune=generic/-march=native -mtune=native/g' /etc/makepkg.conf
