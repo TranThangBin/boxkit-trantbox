9f6c162317f8d521f7a47b5eb8d5e17b8fffcf10
TranThangBin <thangdev04@gmail.com>
2026-07-05 14:18:05 +0700

    refactor: just have the base image with default dependencies

--- scripts/trantbox-cleanup.sh
+++
@@ -1,5 +0,0 @@
-userdel -r build
-rm -drf /home/build
-sed -i '/build ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
-sed -i '/root ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
-rm -rf /tmp/* /var/cache/pacman/pkg/*
