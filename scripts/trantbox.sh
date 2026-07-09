28ac12f8f7f9af23c322c45b0a84e492c5c045b8
TranThangBin <thangdev04@gmail.com>
2026-07-09 13:21:59 +0700

    chore: link dash to sh in script

--- scripts/trantbox.sh
+++ scripts/trantbox.sh
@@ -11,3 +11,5 @@ chmod +x /usr/bin/latencyflex
 yes | pacman -Scc
 rm -rf /tmp/* /var/cache/pacman/pkg/*
 sed -i 's/-march=x86-64 -mtune=generic/-march=native -mtune=native/g' /etc/makepkg.conf
+
+ln -sfT /usr/bin/dash /usr/bin/sh
