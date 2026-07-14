917258b46dda6a08044e21a87c0eb2a0ba6ca9f7
TranThangBin <thangdev04@gmail.com>
2026-07-14 17:20:14 +0700

    fix: try to make latencyflex work

--- scripts/trantbox.sh
+++ scripts/trantbox.sh
@@ -12,10 +12,9 @@ LATENCYFLEX_VERSION=v0.1.1
 LATENCYFLEX_OUT="latencyflex-$LATENCYFLEX_VERSION"
 wget "https://github.com/ishitatsuyuki/LatencyFleX/releases/download/$LATENCYFLEX_VERSION/$LATENCYFLEX_OUT.tar.xz"
 tar -xf "$LATENCYFLEX_OUT.tar.xz"
-cp "$LATENCYFLEX_OUT/layer/usr/lib/x86_64-linux-gnu"/* \
-  /usr/lib/
-cp "$LATENCYFLEX_OUT/layer/usr/share/vulkan/implicit_layer.d"/* \
-  /usr/share/vulkan/implicit_layer.d/
+cp -r "$LATENCYFLEX_OUT/layer/usr/lib/x86_64-linux-gnu"/* /usr/lib/
+cp -r "$LATENCYFLEX_OUT/layer/usr/share"/* /usr/share/
+cp -r "$LATENCYFLEX_OUT/wine"/* /
 sed -i 's@"/usr/lib/x86_64-linux-gnu/liblatencyflex_layer.so"@"/usr/lib/liblatencyflex_layer.so"@g' \
   /usr/share/vulkan/implicit_layer.d/latencyflex.json
 rm -rf "/latencyflex.tar.xz" "/latencyflex/"
