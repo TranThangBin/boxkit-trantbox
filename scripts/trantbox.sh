8ccc31c45866131be290beab504dcbcec4e24b77
TranThangBin <thangdev04@gmail.com>
2026-07-25 21:42:17 +0700

    chore: remove latencyflex and add extra essential packages

--- scripts/trantbox.sh
+++ scripts/trantbox.sh
@@ -5,21 +5,6 @@ ln -sfT /usr/bin/dash /usr/bin/sh
 
 grep -v "^#" ./trantbox.packages | xargs pacman -Syyu --needed --noconfirm
 
-wget https://raw.githubusercontent.com/Shringe/LatencyFleX-Installer/main/install.sh -O /usr/bin/latencyflex
-sed -i 's@"dxvk.conf"@"/usr/share/latencyflex/dxvk.conf"@g' /usr/bin/latencyflex
-chmod +x /usr/bin/latencyflex
-
-LATENCYFLEX_VERSION=v0.1.1
-LATENCYFLEX_OUT="latencyflex-$LATENCYFLEX_VERSION"
-wget "https://github.com/ishitatsuyuki/LatencyFleX/releases/download/$LATENCYFLEX_VERSION/$LATENCYFLEX_OUT.tar.xz"
-tar -xf "$LATENCYFLEX_OUT.tar.xz"
-cp -r "$LATENCYFLEX_OUT/layer/usr/lib/x86_64-linux-gnu"/* /usr/lib/
-cp -r "$LATENCYFLEX_OUT/layer/usr/share"/* /usr/share/
-cp -r "$LATENCYFLEX_OUT/wine"/* /
-sed -i 's@"/usr/lib/x86_64-linux-gnu/liblatencyflex_layer.so"@"/usr/lib/liblatencyflex_layer.so"@g' \
-  /usr/share/vulkan/implicit_layer.d/latencyflex.json
-rm -rf "/latencyflex.tar.xz" "/latencyflex/"
-
 yes | pacman -Scc
 rm -rf /tmp/* /var/cache/pacman/pkg/*
 sed -i 's/-march=x86-64 -mtune=generic/-march=native -mtune=native/g' /etc/makepkg.conf
